bbfe4ff160fcd8dfcc257a2131a179a93aed9908
unknown <polsahebrao@gmail.com>
2014-03-01 16:35:38 +0530

    update

--- classes/SQL.class.php
+++ classes/SQL.class.php
@@ -3,17 +3,36 @@ class SQL{
    private $db_host ;
    private $db_user;
    private $db_password;
-   private $db_table;
+   private $db_name;
 
-    public function __construct($db_host=null,$db_user=null,$db_password=null,$dbdb_table=null) {
+    public function __construct($db_host=null,$db_user=null,$db_password=null,$db_name=null) {
 
         $this->setDbHost($db_host);
         $this->setDbUser($db_user);
         $this->setDbPassword($db_password);
-        $this->setDbTable($dbdb_table);
-        $this->setConnection();
+        $this->setDbName($dbdb_name);
+        return $this->setConnection();
     }
-    private function setDbHost($dbH_host)
+    private function setDbHost($db_host){
+        $this->db_host=$db_host;
+    }
+    private function setDbUser($db_user){
+        $this->db_user=$db_user;
+    }
+    private function setDbPassword($db_password){
+        $this->db_password=$db_password;
+    }
+    private function setDbName($db_name){
+        $this->db_name;
+        mysqli_select_db($this->db_name);
+    }
+    private function setConnection(){
+        $conn;
+        try{
+           $conn= mysqli_connect($this->db_host,$this->db_user,  $this->db_password);
+        }  catch (Exception $e){}
+        return $conn;
+    }
 }
 
 ?>
